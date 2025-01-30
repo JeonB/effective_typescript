1fea76f8a7a47e1def0d6eb3b8a52f84b5fb5877
JeonB <newjeonb@gmail.com>
2025-01-30 15:34:25 +0900

    아이템16 - number 인덱스 시그니처보다는 Array, 튜플, ArrayLike 사용하기

---
+++ src/item16/indexsigniture.ts
@@ -0,0 +1,17 @@
+const xs = [1, 2, 3];
+const x0 = xs[0];
+const x1 = xs["1"]; // 뭔짓을해도 타입체커가 오류라고 인식 못 함.
+
+function checkedAccess<T>(xs: ArrayLike<T>, i: number): T {
+  if (i < xs.length) {
+    return xs[i];
+  }
+
+  throw new Error(`Out of bounds access: ${i}`);
+}
+
+const tupleLike: ArrayLike<string> = {
+  "0": "a",
+  "1": "b",
+  length: 2,
+};
