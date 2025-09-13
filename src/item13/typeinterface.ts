2b3dd655f875a284347f269fbc7ccf05ac10f798
JeonB <newjeonb@gmail.com>
2025-09-13 22:19:30 +0900

    타입과 인터페이스

--- src/item13/typeinterface.ts
+++ src/item13/typeinterface.ts
@@ -1,5 +1,10 @@
 type AorB = "a" | "b"; // 유니온 인터페이스라는 개념은 없음
 
+// 동적 문법은 type에서만 사용 가능
+// console.log 에서 확인 불가. 런타임에 타입은 전부 삭제되므로
+type MappingType = {
+  [key in keyof Istate]: boolean;
+};
 type Pair = [number, number];
 type StringList = string[];
 type NamedNums = [string, ...number[]];
@@ -19,3 +24,9 @@ const city: Istate = {
   capital: "Seoul",
   population: "10,000,000",
 };
+
+const arrtest = [1, 2, 3];
+for (const key in arrtest) {
+  console.log(key);
+  console.log(typeof key);
+}
