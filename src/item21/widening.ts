82b08e891a78e6a88189962c1139788df73e99b3
JeonB <newjeonb@gmail.com>
2025-10-30 22:42:21 +0900

    수정: axis 변수를 let에서 const로 변경하고, const 단언문을 활용한 타입 추론 예제 추가

--- src/item21/widening.ts
+++ src/item21/widening.ts
@@ -10,8 +10,34 @@ const item21 = () => {
   };
 
   const vector = { x: 10, y: 20, z: 30 };
-  let axis = "x";
+  const axis = "x"; // let 으로 선언된 axis변수는 'x'|'y'|'z' 타입 이외에도 다른 타입을 가질 수 있다. const 로 선언된 변수는 타입을 좁혀서 사용할 수 있다.
 
-  const component = getComponent(vector, axis); // let 으로 선언된 axis변수는 'x'|'y'|'z' 타입 이외에도 다른 타입을 가질 수 있다.
+  const component = getComponent(vector, axis);
   console.log(component);
+
+  const obj = { x: 10, y: 20, z: 30 };
+
+  obj.x = "ss"; // number 타입만 할당 가능
+  obj.a = 123; // a 속성이 없으므로 에러
+
+  /* const 단언문으로 타입 추론 강도 조절 */
+  const v1 = {
+    x: 1,
+    y: 2,
+  };
+
+  const v2 = {
+    x: 1 as const,
+    y: 2,
+  };
+
+  const v3 = {
+    x: 1,
+    y: 2,
+  } as const;
+
+  const a1 = [1, 2, 3];
+  const a2 = [1, 2, 3] as const; // const 단언문은 최대한 좁은 타입으로 추론하게 만든다
 };
+
+item21();
